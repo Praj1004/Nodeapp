6bf7de8ab6fdfc6211fa52ede6bf74541b95cc12
praj1004 <prajaktamahamulkar@gmail.com>
2023-07-18 21:25:03 +0530

    hello

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var app = require('../index.js');describe('GET /', function() {
 
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
 
- request(app).get('/').expect('hello all', done);
+ request(app).get('/').expect('hello', done);
 
  });
 
